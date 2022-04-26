a600a970b0471809e4943f73dee9e8fa9c85aa22
dziekan30 <dziekan30@gmail.com>
2022-04-26 08:33:24 -0500

    OOP Java

---
+++ src/com/dziekan30/Main.java
@@ -0,0 +1,12 @@
+package com.dziekan30;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Car porsche = new Car();
+        Car holden = new Car();
+        porsche.setModel("911");
+        System.out.println("model is "+ porsche.getModel());
+
+    }
+}
